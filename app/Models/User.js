9c2a1ae2f46fc18bb18712a3d3aef97811df2ece
Obi NkemjIka Derick <nkemjikaobi@gmail.com>
2021-11-17 13:36:15 +0100

    User Model

---
+++ app/Models/User.js
@@ -0,0 +1,65 @@
+const crypto = require('crypto');
+const mongoose = require('mongoose');
+// user schema
+const UserSchema = new mongoose.Schema(
+	{
+		email: {
+			type: String,
+			required: true,
+			unique: true,
+		},
+		username: {
+			type: String,
+			unique: true,
+			required: true,
+			min: 3,
+		},
+		hashed_password: {
+			type: String,
+			required: true,
+		},
+		salt: {
+			type: String,
+		},
+	},
+	{ timestamps: true }
+);
+
+// virtual
+UserSchema
+	.virtual('password')
+	.set(function (password) {
+		this._password = password;
+		this.salt = this.makeSalt();
+		this.hashed_password = this.encryptPassword(password);
+	})
+	.get(function () {
+		return this._password;
+	});
+
+// methods
+UserSchema.methods = {
+	authenticate: function (plainText) {
+		return this.encryptPassword(plainText) === this.hashed_password; // true false
+	},
+
+	encryptPassword: function (password) {
+		if (!password) return '';
+		try {
+			return crypto
+				.createHmac('sha1', this.salt)
+				.update(password)
+				.digest('hex');
+		} catch (err) {
+			return '';
+		}
+	},
+
+	makeSalt: function () {
+		return Math.round(new Date().valueOf() * Math.random()) + '';
+	},
+};
+
+const User = mongoose.model('user', UserSchema);
+
+module.exports = User;
